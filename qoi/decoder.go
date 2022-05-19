ba4eebbf30671666f398cb6e293a70697d8f65b2
Tyler Kropp <tyler@tylerkropp.xyz>
2022-05-18 21:15:37 -0400

    Add channels and color space decode fail tests

--- qoi/decoder.go
+++ qoi/decoder.go
@@ -3,6 +3,7 @@ package qoi
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"image"
 	"io"
 )
@@ -13,22 +14,44 @@ func Decode(reader io.Reader) (image.Image, error) {
 	magic := make([]byte, 4)
 	err := binary.Read(reader, binary.BigEndian, magic)
 	if err != nil {
-		return image.NewNRGBA(image.Rectangle{}), err
+		return nil, err
 	}
+
 	correctMagic := []byte{'q', 'o', 'i', 'f'}
 	if string(magic) != string(correctMagic) {
-		return image.NewNRGBA(image.Rectangle{}), ErrParseHeader
+		return nil, fmt.Errorf("bad magic bytes: %w", ErrParseHeader)
 	}
+
 	var width uint32
 	err = binary.Read(reader, binary.BigEndian, &width)
 	if err != nil {
-		return image.NewNRGBA(image.Rectangle{}), err
+		return nil, err
 	}
+
 	var height uint32
 	err = binary.Read(reader, binary.BigEndian, &height)
 	if err != nil {
-		return image.NewNRGBA(image.Rectangle{}), err
+		return nil, err
 	}
+
+	var channels uint8
+	err = binary.Read(reader, binary.BigEndian, &channels)
+	if err != nil {
+		return nil, err
+	}
+	if channels != ChannelRGB && channels != ChannelRGBA {
+		return nil, fmt.Errorf("bad channels %v: %w", channels, ErrParseHeader)
+	}
+
+	var colorSpace uint8
+	err = binary.Read(reader, binary.BigEndian, &colorSpace)
+	if err != nil {
+		return nil, err
+	}
+	if colorSpace != ColorSpaceSRGB && colorSpace != ColorSpaceLinear {
+		return nil, fmt.Errorf("bad color space %v: %w", colorSpace, ErrParseHeader)
+	}
+
 	return image.NewNRGBA(image.Rectangle{
 		Min: image.Point{0, 0},
 		Max: image.Point{int(width), int(height)},
--- qoi/decoder_test.go
+++ qoi/decoder_test.go
@@ -66,4 +66,44 @@ func TestDecode(t *testing.T) {
 			t.Fatalf("expected %v but got %v", expectedHeight, actualHeight)
 		}
 	})
+
+	t.Run("Should fail parsing bad channels", func(t *testing.T) {
+		t.Parallel()
+		const width = 2
+		const height = 2
+		reader := bytes.NewReader([]byte{
+			'q', 'o', 'i', 'f', 0, 0, 0, width, 0, 0, 0, height, 9, qoi.ColorSpaceSRGB,
+			0, 0, 0, 0, 0, 0, 0, 1,
+		})
+
+		_, err := qoi.Decode(reader)
+
+		if err == nil {
+			t.Fatal("expected non-nil error")
+		}
+		expected := qoi.ErrParseHeader
+		if !errors.Is(err, expected) {
+			t.Fatalf("expected %q but got %q", expected, err)
+		}
+	})
+
+	t.Run("Should fail parsing bad color space", func(t *testing.T) {
+		t.Parallel()
+		const width = 2
+		const height = 2
+		reader := bytes.NewReader([]byte{
+			'q', 'o', 'i', 'f', 0, 0, 0, width, 0, 0, 0, height, qoi.ChannelRGBA, 2,
+			0, 0, 0, 0, 0, 0, 0, 1,
+		})
+
+		_, err := qoi.Decode(reader)
+
+		if err == nil {
+			t.Fatal("expected non-nil error")
+		}
+		expected := qoi.ErrParseHeader
+		if !errors.Is(err, expected) {
+			t.Fatalf("expected %q but got %q", expected, err)
+		}
+	})
 }
